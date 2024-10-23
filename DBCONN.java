863705c1b062d88df065cc3e1787ca6377ab78da
ritesh329 <145488174+ritesh329@users.noreply.github.com>
2024-10-22 23:03:28 -0800

    Add files via upload

---
+++ DBCONN.java
@@ -0,0 +1,32 @@
+package DATABASECON;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBCONN {
+
+	 private static  Connection connection=null;
+	  
+	  static public Connection getConnection() throws SQLException 
+	  {
+		    
+		  if(connection==null)
+		  {
+			  
+			  try {
+					Class.forName("com.mysql.cj.jdbc.Driver");
+			    
+					connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/INTERNSHIP", "root", "root");
+					System.out.println("sucessfull");
+		
+		  }catch(Exception e)
+			  {
+			         e.printStackTrace();
+			  }
+		  }
+		  
+		  return connection;
+	  }
+	  
+}
